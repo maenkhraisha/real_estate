a1fb89e1e44ed78129b745ab7f6d536359db2b30
Maen KH <m.alkhraisha@sakan.co>
2025-06-06 02:33:39 +0300

    add client

---
+++ client/src/app/(nondashboard)/layout.tsx
@@ -0,0 +1,17 @@
+import Navbar from "@/components/Navbar";
+import { NAVBAR_HEIGHT } from "@/lib/constants";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className='h-full w-full'>
+            <Navbar />
+            <main
+                className={`h-full w-full flex flex-col pt-[${NAVBAR_HEIGHT}]`}
+                style={{ paddingTop: `${NAVBAR_HEIGHT}px` }}>
+                {children}
+            </main>
+        </div>
+    );
+};
+
+export default Layout;
---
+++ client/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div className='bg-red-100'>
+            <p>Home page</p>
+        </div>
+    );
+}
